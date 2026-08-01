04f18e0069c177e477b9a08030fb7735a1024bb3
ludaalaass <adityaji@duck.com>
2026-08-01 13:56:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -64,8 +64,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv("GIT_TOKEN")  # needed if repo is private
 
 # ── Support links ──────────────────────────────────────────────────────────────
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/OSINTNXERA")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+3XobX_t--Bk4YmM1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AHANA_SUPPORTCHANNEL")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+xza-B8vEYB9mMTE1")
 
 # ── Assistant auto-leave ───────────────────────────────────────────────────────
 AUTO_LEAVING_ASSISTANT = False
